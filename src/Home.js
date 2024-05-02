34de435b589098c3b18c69ba799e500fd4f89a24
stianglomsrod <112327355+stianglomsrod@users.noreply.github.com>
2024-05-02 07:48:49 +0200

    Update Home and Navbar components, add state and event handling

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,24 @@
+import { useState } from "react";
+
 const Home = () => {
-    return ( 
-        <div className="home">
-            <h2>Homepage</h2>
-        </div>
-     );
-}
- 
+  // let name = "Stian";
+  const [name, setName] = useState("Stian");
+  const [age, setAge] = useState(35)
+
+  const handleClick = () => {
+    setName("Thomas")
+    setAge("34")
+
+  };
+
+
+  return (
+    <div className="home">
+      <h2>Homepage</h2>
+      <p>{ name } is { age }</p>
+      <button onClick={handleClick}>Click me</button>
+    </div>
+  );
+};
+
 export default Home;

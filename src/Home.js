ece06254a83745b5cd2a91ba60410481e87ecfd5
stianglomsrod <112327355+stianglomsrod@users.noreply.github.com>
2024-05-04 12:45:38 +0200

    Refactor Home component, use useFetch custom hook for API integration

--- src/Home.js
+++ src/Home.js
@@ -1,23 +1,18 @@
 import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState(null);
-
-
-  useEffect(() => {
-    fetch("http://localhost:8000/blogs")
-      .then(res => {
-        return res.json()
-      })
-      .then((data => {
-        console.log(data);
-        setBlogs(data)
-      }))
-  }, []);
+  const {
+    data: blogs,
+    isPending,
+    error,
+  } = useFetch("http://localhost:8000/blogs");
 
   return (
     <div className="home">
+      {error && <div>{error}</div>}
+      {isPending && <div>Loading...</div>}
       {blogs && <BlogList blogs={blogs} title="All blogs!" />}
     </div>
   );

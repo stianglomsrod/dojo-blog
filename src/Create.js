078c3d334411df55396f4db0cc917c227ebe497c
stianglomsrod <112327355+stianglomsrod@users.noreply.github.com>
2024-05-05 08:28:54 +0200

    Refactor Create component, add form for creating new blogs

--- src/Create.js
+++ src/Create.js
@@ -4,12 +4,23 @@ const Create = () => {
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
   const [author, setAuthor] = useState("mario");
+  const [isPending, setIsPending] = useState(false)
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    const blog = { title, body, author }
-    console.log(blog);
-  }
+    const blog = { title, body, author };
+
+    setIsPending(true)
+
+    fetch("http://localhost:8000/blogs", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(blog),
+    }).then(() => {
+      console.log("New blog added");
+      setIsPending(false)
+    });
+  };
 
   return (
     <div className="create">
@@ -34,11 +45,16 @@ const Create = () => {
         ></textarea>
         <label>Blog author:</label>
         <select
-        value={author} onChange={(e) => {setAuthor(e.target.value)}}>
+          value={author}
+          onChange={(e) => {
+            setAuthor(e.target.value);
+          }}
+        >
           <option value="mario">mario</option>
           <option value="yoshi">yoshi</option>
         </select>
-        <button>Add blog</button>
+        {!isPending && <button>Add blog</button>}
+        {isPending && <button disabled>Adding blog...</button>}
       </form>
     </div>
   );
